8b1051df5cb733364f0e419cab56cac5e5251ab8
LinkaG <z.gilvanova@yandex.ru>
2023-01-08 13:50:44 +0500

    add frontend layout with logic parts

--- model_inference_api/api.py
+++ model_inference_api/api.py
@@ -20,9 +20,9 @@ word_dict = get_word_dict()
 app = FastAPI()
 
 origins = [
-    f"{host}:8000",
+    f"{host}:3000",
     "http://localhost",
-    "http://localhost:8000",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
@@ -55,7 +55,10 @@ if mongodb.list_collection_names() == []:
 @app.post("/get_games_numbers", status_code=200)
 async def get_collections_names():
     """Returns numbers of games recorded in db."""
-    result = mongodb.list_collection_names()
+    names = mongodb.list_collection_names()
+    result = []
+    for num, value in enumerate(names):
+        result.append({"number": num, "name": value})
     return result
 
 
@@ -67,7 +70,8 @@ async def get_word_position_in_collection(n_game: str = "game1", word: str = "co
     word_position = []
     for i in result:
         word_position.append(i.get("position"))
-    return word, word_position[0]
+    result_obj = {"word": word, "position": word_position[0]}
+    return result_obj
 
 
 @app.post("/get_first_similar_words", status_code=200)
